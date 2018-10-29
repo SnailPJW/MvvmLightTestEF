ebc52e1f14ff064e8a7d6e0bc2a3c91c683147a9
snailpjw <alanwei1202@gmail.com>
2018-10-29 17:26:54 +0800

    try something about 3.Code first DataAnnotations.

--- MvvmLight/Data/Tables/Patient.cs
+++ MvvmLight/Data/Tables/Patient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -12,8 +13,17 @@ namespace MvvmLight4EF.Data.Tables
         public long PatientId { get; set; }
 
         [Required]
-        public string PatientName { get; set; }
+        public string FirstName { get; set; }
 
+        public string LastName { get; set; }
+
+        //[StringLength(100)]
+        //[MinLength(3),MaxLength(100)]
+        [NotMapped] // computed / Derived fields 
+        public string FullName => string.Format("{0} {1}", FirstName, LastName);
+
+
+        public DateTime? DateOfBirth { get; set; }
         //Collection navigation property
         //public IList<Record> Recoreds { get; set; }
         public virtual ICollection<Record> Record { get; set; }
--- MvvmLight/Data/Tables/Record.cs
+++ MvvmLight/Data/Tables/Record.cs
@@ -7,16 +7,20 @@ namespace MvvmLight4EF.Data.Tables
     public class Record
     {
         //Primary key
-        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]//[Key] identity (auto-increment) column
         public long RecordId { get; set; }
 
         [Required]
+        [Column("StatusOfRecord", TypeName = "varchar", Order = 1)]
         public string RecordStatus { get; set; }
 
+        [Range(0,4)]
+        public int? RecordStatusRange { get; set; }
         //Navigation property
         //public Patient Patient { get; set; }
         //Foreign Key Convention
-        public long PatientId { get; set; }
+        //public long PatientId { get; set; }
+        [ForeignKey("PatientId")]
         public virtual Patient Patient { get; set; }
     }
 }
