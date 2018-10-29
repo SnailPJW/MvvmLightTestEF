691bdce6dbaab11cfe7073f0b36633046d869aa2
snailpjw <alanwei1202@gmail.com>
2018-10-29 13:02:28 +0800

    Add project files.

---
+++ MvvmLight/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MvvmLight.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
