1d5191d61036baacc0c76fb86749e685033b45f9
snailpjw <alanwei1202@gmail.com>
2018-10-29 14:09:26 +0800

    add some comment about mvvmlight

--- MvvmLight/ViewModel/MainViewModel.cs
+++ MvvmLight/ViewModel/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using GalaSoft.MvvmLight;
+﻿using GalaSoft.MvvmLight;//最顶层的命名空间，包含了MvvmLight的主体,最核心的功能都在这里。
+                         //1.ICleanup            接口。实现该接口的ViewModel需要在Cleanup方法中释放资源，特别是-=event
+                         //2.ObservableObject    该类实现了INotifyPropertyChanged接口，定义了一个可通知的对象基类，供ViewModelBase继承
+                         //3.ViewModelBase       继承自ObsevableObject,ICleanup。将作为MvvmLight框架下使用的ViewModel的基类。主要提供Set和RaisePropertyChanged供外部使用。同时会在Cleanup方法里，Unregister该实例的所有MvvmLight Messager（在GalaSoft.MvvmLight.Messaging命名空间内定义）
 using MvvmLight.Model;
 
 namespace MvvmLight.ViewModel
@@ -9,7 +12,8 @@ namespace MvvmLight.ViewModel
     /// See http://www.mvvmlight.net
     /// </para>
     /// </summary>
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel : ViewModelBase//ViewModelBase继承了INotifyPropertyChanged接口并提供了一个Set方法来给属性赋值。
+                                              //（也就是不用自己在VeiwModel实现INotifyProperrtyChanged,然后在属性赋值时通知了，当然也可以手动通知）
     {
         private readonly IDataService _dataService;
 
--- MvvmLight/ViewModel/ViewModelLocator.cs
+++ MvvmLight/ViewModel/ViewModelLocator.cs
@@ -5,6 +5,7 @@
                                    x:Key="Locator" />
   </Application.Resources>
   
+
   In the View:
   DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
 */
@@ -19,6 +20,17 @@ namespace MvvmLight.ViewModel
     /// <summary>
     /// This class contains static references to all the view models in the
     /// application and provides an entry point for the bindings.
+    /// （这个类包含了这个应用中所有ViewModel的静态引用并提供了绑定的进入点。）
+    /// 
+    /// App.xaml中将ViewModelLocator作为资源添加到了全局的Application.Resources里
+    /// 
+    /// 在这个类中，完成了为View创建ViewModel示例的工作。
+    /// 
+    /// 使用ViewModelLocator的好处：
+    /// 1.View和ViewModel之间不再直接引用，而是通过ViewModelLocator关联。
+    /// 2.储存在ViewModelLocator里的ViewModel类似与单例的存在，可以在全局引用绑定。
+    /// 3.避免了某些情况下频繁创建ViewModel，却未做好资源释放造成的内存泄漏。（并不是说所有ViewModel都必须放到ViewModelLocator里）
+    /// 
     /// <para>
     /// See http://www.mvvmlight.net
     /// </para>
@@ -27,18 +39,37 @@ namespace MvvmLight.ViewModel
     {
         static ViewModelLocator()
         {
-            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            //
+            //SimpleIoc:    一个非常简单的依赖注入容器。 Ioc（控制反转）
+            //SimpleIoc.Default：    SimpleIoc的默认实例
+
+            //使用方法：
+            //1.将自己的类注册到SimpleIoc
+            //  SimpleIoc.Default.Register(()=>new MyClass());
+            //
+            //2.把MainViewModel也注册到SimpleIoc
+            //  SimpleIoc.Default.Register<MainViewModel>();
+            //
+            //3.在Main属性中通过ServiceLocator.Current.GetInstance()方法获取实例
+            //  public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+            //
+            //4.在MainViewModel的构造函数中匹配MyClass类
+            //  public MainViewModel(MyClass data)
+            //  {
+            //      WelcomeTitle = data.Name;
+            //  }
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);// 设置用来检索当前实例的委托
 
             if (ViewModelBase.IsInDesignModeStatic)
             {
-                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
+                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();//注册DesignDataService实例（设计时（设计窗口里的界面））
             }
             else
             {
-                SimpleIoc.Default.Register<IDataService, DataService>();
+                SimpleIoc.Default.Register<IDataService, DataService>();//注册DataService实例（运行时）
             }
 
-            SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<MainViewModel>();//注册MainViewModel
         }
 
         /// <summary>
@@ -47,13 +78,15 @@ namespace MvvmLight.ViewModel
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
             "CA1822:MarkMembersAsStatic",
             Justification = "This non-static member is needed for data binding purposes.")]
-        public MainViewModel Main
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
-            }
-        }
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();   //获取MainViewModel的实例(依赖注入)
+        //上一行等效于：
+        //public MainViewModel Main
+        //{
+        //    get
+        //    {
+        //        return ServiceLocator.Current.GetInstance<MainViewModel>();
+        //    }
+        //}
 
         /// <summary>
         /// Cleans up all the resources.
